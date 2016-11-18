4be3422bf6696f343a20013695f6b097ac26d2f6
alexey_razuvaev <r125644@gmail.com>
2016-11-18 11:55:46 +0700

    Add validations to Interview model

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,2 +1,7 @@
 class Interview < ApplicationRecord
+  validates :content_preview,
+            :interviewee_full_name,
+            :interviewee_synopsis,
+            :read_more_link,
+            presence: true
 end
--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Interview, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:content_preview) }
+    it { is_expected.to validate_presence_of(:interviewee_full_name) }
+    it { is_expected.to validate_presence_of(:interviewee_synopsis) }
+    it { is_expected.to validate_presence_of(:read_more_link) }
+  end
 end

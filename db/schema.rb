cceb53a41a57dab79b016413329a8ccab7e18282
alexey_razuvaev <r125644@gmail.com>
2016-11-18 19:08:23 +0700

    Implement Testimonial model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118063006) do
+ActiveRecord::Schema.define(version: 20161118114637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20161118063006) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.text     "image_data"
+  end
+
+  create_table "testimonials", force: :cascade do |t|
+    t.text     "content"
+    t.string   "reader_full_name"
+    t.string   "reader_link"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end

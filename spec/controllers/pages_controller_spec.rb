024c4a84d2d220585a7237801eaaa2fa2536d478
alexey_razuvaev <r125644@gmail.com>
2016-11-18 12:11:00 +0700

    Modify pages controller specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   render_views
 
-  describe "static pages" do
-    specify "success response" do
-      pages = [:home, :ambassador, :buy, :impressum, :solar_panel_art]
+  specify "success response" do
+    pages = [:home, :ambassador, :buy, :impressum, :solar_panel_art]
 
-      pages.map do |page|
-        get page
-        expect(response).to have_http_status(:success)
-      end
+    pages.map do |page|
+      get page
+
+      expect(response).to have_http_status(:success)
+    end
     end
   end
 end

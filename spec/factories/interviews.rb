be2fad18b5cc42cf5aac8392edaa44536d6afcec
alexey_razuvaev <r125644@gmail.com>
2016-11-18 12:19:28 +0700

    Annotate models

--- spec/factories/interviews.rb
+++ spec/factories/interviews.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: interviews
+#
+#  id                    :integer          not null, primary key
+#  content_preview       :text
+#  interviewee_full_name :string
+#  interviewee_synopsis  :string
+#  read_more_link        :string
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
+
 FactoryGirl.define do
   factory :interview do
     content_preview 'Lorem Ipsum'

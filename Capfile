771f37111f99dc16b9946e12d07fcdc96e6cd0e1
alexey_razuvaev <r125644@gmail.com>
2016-12-16 22:22:52 +0700

    Setup Capistrano deployment

---
+++ Capfile
@@ -0,0 +1,18 @@
+# Load DSL and set up stages
+require 'capistrano/setup'
+
+# Include default deployment tasks
+require 'capistrano/deploy'
+
+require 'capistrano/scm/git'
+install_plugin Capistrano::SCM::Git
+
+# Includes rails goodies
+require 'capistrano/rvm'
+require 'capistrano/bundler'
+require 'capistrano/rails'
+require 'capistrano/faster_assets'
+require 'capistrano/puma'
+
+# Load custom tasks from `lib/capistrano/tasks` if you have any defined
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
---
+++ config/deploy.rb
@@ -0,0 +1,60 @@
+# config valid only for current version of Capistrano
+lock '3.7.0'
+
+PROVISION_VARS = YAML.load_file('config/provision/group_vars/all')
+
+set :application, PROVISION_VARS['app_name']
+set :repo_url, 'git@github.com:alexeyrazuvaev/the-beam.git'
+set :user, PROVISION_VARS['deploy_user']
+
+# Default branch is :master
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+set :use_sudo, false
+set :deploy_via, :remote_cache
+set :ssh_options, forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa)
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :linked_dirs, %w{ log tmp/pids tmp/cache tmp/sockets public/uploads }
+append :linked_files, '.env'
+
+# For capistrano-puma
+set :puma_init_active_record, true
+
+# For capistrano-bundler
+set :bundle_path, -> { shared_path.join('vendor', 'bundle') }
+set :bundle_flags, '--deployment'
+
+# some tweaks
+namespace :deploy do
+  desc 'Make sure local git is in sync with remote.'
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)}`
+        puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
+        puts 'Run `git push` to sync changes.'
+        exit
+      end
+    end
+  end
+
+  desc 'Create db if needed'
+  task :createdb do
+    on roles(:app) do
+      within release_path do
+        execute :bundle, "exec rake RAILS_ENV=#{fetch(:rails_env)} db:create"
+      end
+    end
+  end
+
+  desc 'Copy production version of .env file'
+  task :copy_env_file do
+    on roles(:app) do
+      env_file_path = ENV['env_file_path']
+      upload!(env_file_path, "#{shared_path}/.env") if env_file_path
+    end
+  end
+
+  before :starting, :check_revision
+  before 'check:linked_files', :copy_env_file
+  before :migrate, :createdb
+end
